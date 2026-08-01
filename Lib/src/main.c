ba9bec8818824270ddebd2f2f0bfe89be9d8eaac
Qunerum <qunerumofficial@gmail.com>
2026-08-01 10:39:40 +0200

    update 2026-08-01 10:39

--- Lib/src/main.c
+++ Lib/src/main.c
@@ -2,8 +2,9 @@
 
 void Init() {
     qgSetRenderType(QGPU_RENDER_TYPE_NO_LIGHT);
-    qgSetFontData(4, QGPU_FONT_STYLE_REGULAR, 1, 1, 1, 1);
+    qgSetFontData(3, QGPU_FONT_STYLE_REGULAR, 1, 1, 1, 1);
 
+    qgConvertFont("fontsReadable/symbols.qfr", "fonts/symbols.qf");
     qgLoadFont("fonts/polish.qf");
     qgLoadFont("fonts/symbols.qf");
 }
@@ -15,12 +16,15 @@ void Update() {
     qgAddIndex(qgAddVertex(100, -86.602,  0,  0, 0, 1, 1));
     */
 
-    qgAddText(-400, 200, 0, "\
+    qgAddText(-qgGetWidth() / 2.0f + 10, qgGetHeight() / 2.0f - 10, 0, "\
 `1234567890-= []\\ ;' ,./\n\
 ~!@#$%^&*()_+ {}| :\" <>?\n\
 ABCDEFGHIJKLMNOPQRSTUVWXYZ\n\
 abcdefghijklmnopqrstuvwxyz\n\
+\n\
 \x80 \x81 \x82 \x83 \x84 \x85 \x86 \x87 \x88 \x89 \x8A \x8B \x8C\n\
+\x8D \x8E \x8F \x90 \x91 \x92 \x93 \x94 \x95 \x96 \x97 \x98 \x99\n\
+\n\
 \xB3 \xB4 \xB5 \xB6 \xB7 \xB8 \xB9 \xBA \xBB\n\
 \xBC \xBD \xBE \xBF \xC0 \xC1 \xC2 \xC3 \xC4");
 }
